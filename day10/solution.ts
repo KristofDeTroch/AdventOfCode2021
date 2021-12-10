0e6ea8e46fbf83de28fa28a49213c17e6eb16eb7
KristofDeTroch <detroch.kristof@gmail.com>
2021-12-10 09:17:26 +0100

    day10 two stars

--- day10/solution.ts
+++ day10/solution.ts
@@ -2,21 +2,34 @@ import fs from 'fs';
 
 const openC = [..."([{<"]
 const closeC = [...")]}>"]
+const part1Scores: any = {
+  ')': 3,
+  ']': 57,
+  '}': 1197,
+  '>': 25137
+}
+const part2Scores: any = {
+  '(': 1,
+  '[': 2,
+  '{': 3,
+  '<': 4
+}
 
 fs.readFile('day10/input.txt', 'utf-8', (_err: any, input: any) => {
   const data: string[] = input.split('\n');
-  let score = 0;
+  let score1 = 0;
+  let score2: number[] = [];
   for (let i = 0; i < data.length; i++) {
     let line = [...data[i]];
-    let nextChar: number = -1;
+    let nextChar: string = '';
     while (line.length > 0) {
       let corrupt = false;
       let end = true;
       for (let j = 0; j < line.length - 1; j++) {
         let currentChar: number = openC.indexOf(line[j]);
-        nextChar = closeC.indexOf(line[j + 1]);
-        if (nextChar != -1) {
-          if (currentChar == nextChar) {
+        nextChar = line[j + 1];
+        if (closeC.indexOf(nextChar) != -1) {
+          if (currentChar == closeC.indexOf(nextChar)) {
             line.splice(j, 2);
             end = false
             break;
@@ -28,30 +41,24 @@ fs.readFile('day10/input.txt', 'utf-8', (_err: any, input: any) => {
       }
       if (corrupt) {
 
-        switch (nextChar) {
-          case 0:
-            score += 3;
-            break;
-          case 1:
-            score += 57;
-            break;
-          case 2:
-            score += 1197;
-            break;
-          case 3:
-            score += 25137;
-            break;
-          default:
-            console.log("ERROR:");
-            console.log(data[i])
-            console.log(line.join(''))
-
-        }
+        score1 += part1Scores[nextChar]
         break;
       }
-      if (end) { break; }
+      if (end) {
+        let s = 0;
+        for (let i = line.length - 1; i >= 0; i--) {
+          s *= 5;
+          s += part2Scores[line[i]]
+        }
+        score2.push(s);
+        break;
+      }
     }
 
   }
-  console.log(score)
+  console.log(score1)
+
+  score2.sort((a: number, b: number) => a - b);
+  console.log(score2[(score2.length - 1) / 2])
 });
+
