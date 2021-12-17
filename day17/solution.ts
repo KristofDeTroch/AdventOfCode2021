cb34903ba01d6f751d6ed0f2e2676d1191edab60
KristofDeTroch <detroch.kristof@gmail.com>
2021-12-17 12:08:35 +0100

    day 17 one star

---
+++ day17/solution.ts
@@ -0,0 +1,46 @@
+import fs from 'fs';
+
+fs.readFile('day17/input.txt', 'utf-8', (_err: any, input: any) => {
+  let data = input.slice(input.indexOf('=') + 1);
+  let splitIndex = data.indexOf('.')
+  let minX = parseInt(data.slice(0, splitIndex))
+  let maxX = parseInt(data.slice(splitIndex + 2, data.indexOf(',')))
+
+  data = data.slice(data.indexOf('=') + 1);
+  splitIndex = data.indexOf('.')
+  let minY = parseInt(data.slice(0, splitIndex))
+  let maxY = parseInt(data.slice(splitIndex + 2, data.indexOf('\n')))
+
+  const targetX = [minX, maxX];
+  const targetY = [minY, maxY];
+  console.log(targetX, targetY)
+  let highest = 0;
+  for (let x = 1; x < 200; x++) {
+    for (let y = 0; y < 20000; y++) {
+      let velX = x;
+      let velY = y;
+      let currentHighest = 0;
+      let currentXPos = 0;
+      let currentYPos = 0;
+      while (currentYPos >= targetY[0] && currentXPos <= targetX[1]) {
+        if (currentYPos <= targetY[1] && targetX[0] <= currentXPos) {
+          if (currentHighest > highest) {
+            highest = currentHighest;
+          }
+          break;
+        }
+        currentXPos += velX;
+        currentYPos += velY;
+        if (velX != 0) {
+          velX -= 1
+        }
+        velY -= 1;
+        if (currentYPos > currentHighest) {
+          currentHighest = currentYPos;
+        }
+      }
+    }
+  }
+  console.log(highest)
+
+});
